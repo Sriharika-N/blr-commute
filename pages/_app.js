687eb3e33eb1962dbf9a9ad97fd9d6774cf54b81
Sriharika-N <sriharikaiimk@gmail.com>
2026-09-18 22:19:40 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <title>BLR Commute Intelligence</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
