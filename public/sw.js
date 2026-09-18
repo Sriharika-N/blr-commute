013089d42a8898af51d901922ab7c878c07d3351
Sriharika-N <sriharikaiimk@gmail.com>
2026-09-18 22:19:53 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,9 @@ self.addEventListener('push', function (event) {
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: '/icon-192.png'
+      icon: '/icon-192.png',
+      badge: '/icon-192.png',
+      vibrate: [200, 100, 200]
     })
   );
 });
